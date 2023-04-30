f3619d8713d92a949b27397387606b87322d2b98
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-04-30 14:53:36 +0700

    example function in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    sayHello()
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun sayHello() {
+    println("Hello Ryan..")
 }
