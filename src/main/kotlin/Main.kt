466f6b15e2c9a2b438cd4b6ec136ae61d85fb24d
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-04-30 15:05:35 +0700

    example variable in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,16 @@ fun main(args: Array<String>) {
 }
 
 fun sayHello() {
-    println("Hello Ryan..")
+    // Variable cannot be reassigned (val)
+    val name = "Ryan"
+
+    // Error
+    // name = "Hasbie"
+
+    // Variable that can be reassigned
+    // Mutable variable (var)
+    var message = "first message"
+    message = "This is message pass by reassign"
+
+    println("Hello $name, $message")
 }
