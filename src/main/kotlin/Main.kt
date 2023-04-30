fe70130bc3f2f99febdb49cbef8e3e77ede66e45
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-04-30 17:29:05 +0700

    Add init for automation call function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,15 @@ class Person (val name: String, val age: Int) {
     fun greeting() {
         println("Hello my name is $name")
     }
+    // Run automation fun greeting use init
+    init {
+        greeting()
+    }
 }
 
 fun main(args: Array<String>) {
     val person = Person("Ryan", 21)
-    person.greeting()
+    // No need to call
+    // person.greeting()
     println("My age is ${person.age}")
 }
